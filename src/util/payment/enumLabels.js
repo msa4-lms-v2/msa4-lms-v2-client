9ffe7f44d108f0378198a149892268fa39938ca5
HongD93 <tigerson93@gmail.com>
2026-08-15 02:40:23 +0900

    등록금 결제와 자퇴 환불 정보 표시 추가

--- src/util/payment/enumLabels.js
+++ src/util/payment/enumLabels.js
@@ -22,3 +22,27 @@ export const HEALTH_STATUS_VARIANT = {
   UP: 'success',
   DOWN: 'fail',
 };
+
+export const PAYMENT_STATUS_LABEL = {
+  SUCCEEDED: '결제 완료',
+  FAILED: '결제 실패',
+};
+
+export const PAYMENT_STATUS_VARIANT = {
+  SUCCEEDED: 'success',
+  FAILED: 'fail',
+};
+
+export const REFUND_STATUS_LABEL = {
+  REQUESTED: '처리중',
+  SUCCEEDED: '완료',
+  FAILED: '실패',
+  RETRYING: '재시도중',
+};
+
+export const REFUND_STATUS_VARIANT = {
+  REQUESTED: 'processing',
+  SUCCEEDED: 'success',
+  FAILED: 'fail',
+  RETRYING: 'warning',
+};
